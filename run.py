fb4ff228f1fa2a8b275bee201517a5f97cd01c8d
zanettiprado <>
2023-05-29 21:30:05 +0000

    create the idea of the game basing on the assesment guide

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+#battleship game
+
+#insert matrices 
+
+#display the game board for player and computer 
+
+#places the player boats
+
+#place computer boats
+
+#shots from the player 
+
+#shots from the computer 
+
+#ships remaining
+
+#end game 
+
+
