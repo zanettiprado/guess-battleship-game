77cc76dff0475cf4a98e4d5268705767a8eefa02
zanettiprado <>
2023-05-30 21:16:02 +0000

    create the scope for the game define the player computer and first steps for board

--- run.py
+++ run.py
@@ -1,12 +1,33 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
 
 #battleship game
 
+# welcome game 
+
+print("Welcome to Guess Battleship Game!")
+print("All you have to do is find the computers boat and sinks them")
+print("Follow the instructions and good luck.")
+    
 #insert matrices 
 
+player = []
+computer = []
+computer_hidden= []
+
 #display the game board for player and computer 
+for x in range(4):
+    player.append(["O"] * 6)
+for y in range(4):
+    computer.append(["O"] * 6)
+for z in range(4):
+    computer_hidden.append(["O"] * 6)
+
+print(player)
+print(computer)
+print(computer_hidden)
+
+
+# Nessa função eu arrumo a matriz para parecer um tabuleiro de batalha naval
 
 #places the player boats
 
